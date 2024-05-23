d503b50b88221eb03815f264f187ed3881020d25
cmmagalhaes9 <cmmagalhaes9@hotmail.com>
2024-05-23 21:26:15 +0100

    PORT
    Signed-off-by: cmmagalhaes9 <cmmagalhaes9@hotmail.com>

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   MONGODB_URI: process.env.MONGODB_URI,
-  PORT: process.env.port || 3000,
+  PORT: process.env.PORT || 3000,
   SECRET: process.env.SECRET || '12345678910',
   DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
   DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
